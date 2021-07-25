0e14823d2ec6a21fa708d752de5375e39dfec660
Greg Kreisman <gkreisman_cep@qq.com>
2021-07-25 09:02:37 +0000

    added koch function

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ let mj;
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 400);
   angleMode(DEGREES);
+  background(255);
 
-  greg = new Gurtle(width/2,height/2,color(255,0,0));
-  mj = new Gurtle(width/4, height/4, color(0,0,255));
-  sqr(greg);
-  sqr(mj);
+  greg = new Gurtle(10,height/2,color(255,0,0));
+  mj = new Gurtle(10, height-10, color(0,0,255));
+  //sqr(greg);
+  //sqr(mj);
+  koch(greg, 3, 500);
+  koch(mj,3,500);
 }
 
 function instru(){
@@ -31,4 +34,21 @@ function sqr(obj){
     obj.forward(100)
     obj.right(90)
   }
+}
+
+
+function koch(t, order, size){
+  if (order == 0){
+    t.forward(size);
+
+  }else{
+    koch(t, order-1, size/3)   // Go 1/3 of the way
+    t.left(60)
+    koch(t, order-1, size/3)
+    t.right(120)
+    koch(t, order-1, size/3)
+    t.left(60)
+    koch(t, order-1, size/3)
+  }
+
 }
